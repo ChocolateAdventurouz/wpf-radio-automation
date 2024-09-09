ff0cf67418a9e82de338915bafbec4be0b43f268
Karapatakis Aggelos <aggeloskarapatakis@hotmail.com>
2024-09-09 22:15:52 +0300

    SQL Server Config: Temporarily changed string
    Temporarily changed string till it is automatically managed

--- RA.ConsoleApp/DbContextFactory.cs
+++ RA.ConsoleApp/DbContextFactory.cs
@@ -10,7 +10,8 @@ namespace RA.ConsoleApp
 {
     public class DbContextFactory : IDbContextFactory<AppDbContext>
     {
-        private static String connString = "server=localhost;Port=3306;database=ra_prod1;user=root;password=";
+        //todo: customize connection string
+        private static String connString = "server=localhost;Port=6668;database=ra_prod1;user=root;password=12345678";
         public AppDbContext CreateDbContext()
         {
             DbContextOptionsBuilder<AppDbContext> optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
