34db814341f29cdfff530f9e6db0d2e54281cdae
Karapatakis Aggelos <aggeloskarapatakis@hotmail.com>
2024-09-10 21:15:10 +0300

    RA.Database: Auto-create database in migration script

--- RA.Database/MigrationScript.sql
+++ RA.Database/MigrationScript.sql
@@ -1,4 +1,7 @@
-﻿CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
+﻿CREATE DATABASE IF NOT EXISTS 'ra_prod1';
+USE 'ra_prod1';
+
+CREATE TABLE IF NOT EXISTS `__EFMigrationsHistory` (
     `MigrationId` varchar(150) CHARACTER SET utf8mb4 NOT NULL,
     `ProductVersion` varchar(32) CHARACTER SET utf8mb4 NOT NULL,
     CONSTRAINT `PK___EFMigrationsHistory` PRIMARY KEY (`MigrationId`)
